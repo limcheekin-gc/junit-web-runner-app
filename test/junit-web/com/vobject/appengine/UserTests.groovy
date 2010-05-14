047370d2fd37e61a61c6d957ba1103bffd5c916e
limcheekin <limcheekin@localhost>
2010-05-14 17:12:04 +0800

    First import

---
+++ test/junit-web/com/vobject/appengine/UserTests.groovy
@@ -0,0 +1,17 @@
+package com.vobject.appengine
+
+import grails.test.*
+
+class UserTests extends GroovyTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+		assertEquals(1, 1)
+    }
+}
